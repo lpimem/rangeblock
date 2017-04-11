2cf9e299936fc547070ad7e9fb7789955268c025
Lei Pi <lpi@memphis.edu>
2017-04-10 22:45:20 -0500

    update api

--- dist/index.js
+++ dist/index.js
@@ -8,4 +8,6 @@ __export(require("./core/range_meta"));
 __export(require("./core/range_cache"));
 __export(require("./core/dimension"));
 __export(require("./core/node_measure"));
+var node_measure_impl_1 = require("./core/node_measure_impl");
+exports.computeLayout = node_measure_impl_1.computeLayout;
 __export(require("./core/selector"));
