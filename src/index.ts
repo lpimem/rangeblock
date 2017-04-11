71383325be07e0eef02284481f0ef0aaa0866b37
Lei Pi <lpi@memphis.edu>
2017-04-10 22:36:30 -0500

    expose API computeDimension

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from "./core/range_meta";
 export * from "./core/range_cache";
 export * from "./core/dimension";
 export * from "./core/node_measure";
+export {computeLayout} from "./core/node_measure_impl";
 export * from "./core/selector";
